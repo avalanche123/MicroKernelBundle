9012b701415de3cd74f148c5a142dd07dfd0b824
Bulat (Hacker) Shakirzyanov <mallluhuct@gmail.com>
2010-05-14 14:52:55 -0400

    successfully loads action

--- example/index.php
+++ example/index.php
@@ -1,12 +1,30 @@
 <?php
 
-require_once __DIR__.'/../src/Bundle/MicroKernelBundle/HttpServerKernel.php';
+require_once '../autoload.php';
 
-$kernel = new HttpServerKernel('prod', false);
+$loader = new \Symfony\Foundation\UniversalClassLoader();
+$loader->registerNamespaces(array(
+    'Bundle\MicroKernelBundle' => __DIR__ . '/../src/Bundle/MicroKernelBundle',
+));
+$loader->register();
 
-$kernel->get('/user/:username',
-  function(Request $request, Container $container, array $params = array()) {
-    return 'Hello ' . $params['username'];
+use Symfony\Foundation\UniversalClassLoader;
+use Symfony\Components\DependencyInjection\Container;
+use Symfony\Components\HttpKernel\Request;
+use Bundle\MicroKernelBundle\HttpServerKernel;
+
+$kernel = new HttpServerKernel('prod', true);
+$kernel->setConfigPath('config/config_prod.yml');
+$kernel->addBundle('Symfony\Foundation\Bundle\KernelBundle');
+$kernel->addBundle('Symfony\Framework\WebBundle\Bundle');
+$kernel->addBundle('Symfony\Framework\ProfilerBundle\Bundle');
+$kernel->addBundle('Symfony\Framework\ZendBundle\Bundle');
+$kernel->addBundle('Symfony\Framework\SwiftmailerBundle\Bundle');
+$kernel->addBundle('Symfony\Framework\DoctrineBundle\Bundle');
+
+$kernel->get('user_greeting', '/user/:username',
+  function(Request $request, Container $container, $username) {
+    return 'Hello ' . $username;
   }, array('username' => 'Bulat'));
 
-echo $kernel->handle();
+echo $kernel->handle(null);
